7dca35e4a36f1f12a31d6866bfd801ac73e3f2bb
Deepak Singh Ketheth <deepaksinghketheth@gmail.com>
2024-09-03 12:44:00 +0800

    implemented navbar and code to connect to blockchain wallet

--- src/App.js
+++ src/App.js
@@ -15,8 +15,33 @@ import config from './config.json';
 
 function App() {
 
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async () => {
+
+    //Ether provider
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // console.log(provider);
+
+
+    //If user updates the account on metamask, this method will automatically update the account
+    //address in the state & ultimately on the UI too.
+    window.ethereum.on('accountsChanged', async () => {
+      console.log('Hey');
+      const accounts = await window.ethereum.request({method : 'eth_requestAccounts'});
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
+    })
+  }  
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
   return (
     <div>
+
+      <Navigation account={account} setAccount={setAccount}/>
 
       <div className='cards__section'>
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,57 @@ import logo from '../assets/logo.svg';
 
 const Navigation = ({ account, setAccount }) => {
 
+
+    // Gets the accounts from the ethereum provider & saves the first account into function state
+    // Setting the state in child component will automatically save the state in parent component
+    // Hence once the account is connected, it will show the account address instead of 'Connect' button
+    const connectHandler = async () => {
+        
+        //Get Metamask accounts
+        const accounts = await window.ethereum.request({ method : 'eth_requestAccounts'});
+        // console.log(accounts);
+
+        setAccount(accounts[0]); //Sets account to the state
+        // console.log(account) //Gets account from the state
+    }  
+
+
+    return (
+        <nav>
+            <ul className='nav__links'>
+                <li><a href='#'>Buy</a></li>
+                <li><a href='#'>Rent</a></li>
+                <li><a href='#'>Sell</a></li>
+            </ul>
+
+            <div className='nav__brand'>
+                <img src={logo} alt='Logo'></img>
+                <h1>Millow</h1>
+            </div>
+        
+
+            {/* If account exists, it will slice account address & show it
+            If it doesn't exist, it will show connect button.
+            When connect button is clicked, it will call connectHandler method */}
+
+            {account ? (
+                <button
+                    type='button'
+                    className='nav__connect'
+                >
+                    {account.slice(0,6) + '...' + account.slice(38, 42)}
+                </button>
+            ) : (
+                <button
+                    type='button'
+                    className='nav__connect'
+                    onClick={connectHandler}
+                >
+                    Connect
+                </button>
+            )}
+        </nav>
+    )
 }
 
 export default Navigation;
